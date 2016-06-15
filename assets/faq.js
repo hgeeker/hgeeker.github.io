9874d02a2be7b78ee1cd213996fbfb1421f7aa55
hedaiyong <hedy02@vanke.net.cn>
2016-06-15 17:26:03 +0800

    default

--- assets/faq.js
+++ assets/faq.js
@@ -1,3 +1,20 @@
+var floatBtn = document.getElementById('float-btn');
+window.onscroll = function() {
+    var top = document.documentElement.scrollTop || document.body.scrollTop;
+    var header = document.getElementById("nav-header");
+    var trans = top / 200;
+    if (trans >= 0.9) {
+        trans = 0.9;
+    }
+    header.style.backgroundColor = "rgba(255,255,255," + trans + ")";
+    if (trans >= 0.9) {
+        header.style.boxShadow = "0 0 5px #e5e5e5";
+        addClass(floatBtn, "float-btn-opp");
+    } else {
+        header.style.boxShadow = "0 0 0 #e5e5e5";
+        removeClass(floatBtn, "float-btn-opp");
+    }
+}
 var checkItem = function(number) {
     var product = document.getElementById('about-product');
     var order = document.getElementById('about-order');
